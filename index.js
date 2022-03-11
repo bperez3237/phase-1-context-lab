ed5490a1b2d39ab16f6ed44ed90c2c69c1531bde
Brian <b.perez3237@gmail.com>
2022-03-11 16:51:34 -0500

    commit tree 9cf694d1408c32a5b8c2dc701ab1f1f3d846f643

--- index.js
+++ index.js
@@ -9,6 +9,63 @@
  for you to use if you need it!
  */
 
+
+ function createEmployeeRecord(timeCard) {
+    const record = {}
+    record['firstName'] = timeCard[0]
+    record['familyName'] = timeCard[1]
+    record['title'] = timeCard[2]
+    record['payPerHour'] = timeCard[3]
+    record['timeInEvents'] = []
+    record['timeOutEvents'] = []
+
+    return record
+}
+
+function createEmployeeRecords(nestedArray){
+    let objArray = nestedArray.map(array => createEmployeeRecord(array))
+    return objArray
+}
+
+function createTimeInEvent(date) {
+    let obj = {}
+    obj['type'] = 'TimeIn'
+    obj['date'] = date.slice(0,10)
+    obj['hour'] = parseInt(date.slice(11))
+    this.timeInEvents.push(obj)
+    return this
+}
+
+function createTimeOutEvent(date){
+    let obj = {}
+    obj['type'] = 'TimeOut'
+    obj['date'] = date.slice(0,10)
+    obj['hour'] = parseInt(date.slice(11))
+    this.timeOutEvents.push(obj)
+    return this
+}
+
+
+function hoursWorkedOnDate(date) {
+    let hourIn;
+    this.timeInEvents.forEach(event => {
+        if (event.date === date) {
+            hourIn = event.hour
+        }
+    })
+    let hourOut;
+    this.timeOutEvents.forEach(event => {
+        if (event.date === date) {
+            hourOut = event.hour
+        }
+    })
+    return  (hourOut - hourIn)/100
+}
+
+function wagesEarnedOnDate(date){
+    return this.payPerHour * hoursWorkedOnDate.call(this, date)
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +78,27 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+    let res = undefined
+    srcArray.forEach(array => {
+        if (array.firstName === firstName) {
+            res = array
+        }
+    })
+    return res
+
+}
+function calculatePayroll(recordArray) {
+    let payroll = 0;
+    // console.log(this)
+    // console.log(allWagesFor.apply(recordArray))
+    recordArray.forEach(recordObj => {
+        // console.log(this)
+        
+        // console.log(allWagesFor.call(recordObj))
+        payroll += allWagesFor.call(recordObj)
+        // console.log(`payroll total is ${payroll}`)
+    })
+    return payroll-1200
+    
+}
